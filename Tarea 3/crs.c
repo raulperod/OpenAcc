8ef80fdd5a267093aaf1c809bde7d075d22802b4
Raul Perez <raulperod@gmail.com>
2017-09-11 16:08:19 -0700

    llenar row_ptr e imprimir

--- Tarea 3/crs.c
+++ Tarea 3/crs.c
@@ -4,7 +4,7 @@
 
 int main(){
 
-    int matriz[N][M], nnz = 0, contador = 0;
+    int matriz[N][M], nnz = 0, contador = 0, indice = 0, fila = -1;
     int *val, *col_ind, *row_ptr;
     memset(matriz, 0, N*M*sizeof(int));
     // llenar la matriz
@@ -37,17 +37,30 @@ int main(){
     }
     // llenar row_ptr
     row_ptr = (int*)malloc( (N+1) * sizeof(int) );
-    contador = 0;
+    contador = 0; // contador = 0, indice = 0, fila = -1
     for(int i=0 ; i < N ; i++){
         for(int j=0; j < M; j++){
             if( matriz[i][j] != 0){
-                row_ptr[contador] = matriz[i][j];
                 contador++;
+                if( i != fila){
+                    row_ptr[indice] = contador;
+                    fila = i;
+                    indice++;
+                }
             }
         }
     }
-    // imprimir
+    row_ptr[N] = nnz+1;
+    // imprimir val
     for(int i=0; i < nnz; i++){
-        printf("val[%d] = %d , col_ind[%d] = %d\n", i, val[i], i, col_ind[i]);
+        printf("val[%d] = %d\n", i, val[i]);
+    }
+    // imprimir col_ind
+    for(int i=0; i < nnz; i++){
+        printf("col_ind[%d] = %d\n", i, col_ind[i]);
+    }
+    // imprimir row_ptr
+    for(int i=0; i < (N+1); i++){
+        printf("row_ptr[%d] = %d\n", i, row_ptr[i]);
     }
 }
