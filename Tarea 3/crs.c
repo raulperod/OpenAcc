3b95048cf129826957e5f9d7f8a4a144c116f8af
Raul Perez <raulperod@gmail.com>
2017-09-17 11:01:02 -0700

    Para calcular tiempo

--- Tarea 3/crs.c
+++ Tarea 3/crs.c
@@ -1,35 +1,38 @@
 #include <stdio.h>
-#define N 3
-#define M 4 
+#include <stdlib.h>
+#include "timer.h"
+#define N 500
+#define M 500
 
 int main(){
 
-    int matriz[N][M], nnz = 0, contador = 0, indice = 0, fila = -1;
+    int matriz[N*M], nnz = 0, contador = 0, indice = 0, fila = -1;
     int *val, *col_ind, *row_ptr, *u, *resultado;
     memset(matriz, 0, N*M*sizeof(int));
     // llenar la matriz
     for(int i=0 ; i < N ; i++){
         for(int j=0; j < M; j++){
-            if( i == j || j > i){
-                matriz[i][j] = 1;
+            if( i==j ){
+                matriz[j+i*M] = 1;
             }
         }
     }
     // contar los no ceros (nnz)
     for(int i=0 ; i < N ; i++){
         for(int j=0; j < M; j++){
-            if( matriz[i][j] != 0){
+            if( matriz[j+i*M] != 0){
                 nnz++;
             }
         }
     }
+    StartTimer();
     // llenar val y col_ptr
     val = (int*)malloc( nnz * sizeof(int));
     col_ind = (int*)malloc( nnz * sizeof(int) );
     for(int i=0 ; i < N ; i++){
         for(int j=0; j < M; j++){
-            if( matriz[i][j] != 0){
-                val[contador] = matriz[i][j];
+            if( matriz[j+i*M] != 0){
+                val[contador] = matriz[j+i*M];
                 col_ind[contador] = j+1;
                 contador++;
             }
@@ -40,7 +43,7 @@ int main(){
     contador = 0; // contador = 0, indice = 0, fila = -1
     for(int i=0 ; i < N ; i++){
         for(int j=0; j < M; j++){
-            if( matriz[i][j] != 0){
+            if( matriz[j+i*M] != 0){
                 contador++;
                 if( i != fila){
                     row_ptr[indice] = contador;
@@ -52,6 +55,7 @@ int main(){
     }
     row_ptr[N] = nnz+1;
     // imprimir val
+    /*
     for(int i=0; i < nnz; i++){
         printf("val[%d] = %d\n", i, val[i]);
     }
@@ -63,22 +67,43 @@ int main(){
     for(int i=0; i < (N+1); i++){
         printf("row_ptr[%d] = %d\n", i, row_ptr[i]);
     }
+    */
     // ---------------------------------------------
     // lleno el vector
     u = (int*)malloc( M * sizeof(int));
     for(int i=0;i<M;i++){
-        u[i] = i+1;
+        u[i] = 1;
     }
     // Multiplicacion Matriz * u
     resultado = (int*)malloc( N * sizeof(int));
-    for(int i=0, indice=0 ; i < N  ; i++){
-        resultado[i] = 0;
-        for(int j=0 ; j < (row_ptr[i+1] - row_ptr[i]) ; j++, indice++){
-            resultado[i] += val[indice] * u[ col_ind[indice]-1 ];
+    #pragma acc data copyin(val[0:nnz],u[0:N],col_ind[0:nnz],row_ptr[0:N+1]) copyout(resultado[0:N])
+    {
+    #pragma acc parallel
+    {   
+        indice=0;
+        #pragma acc loop
+        for(int i=0 ; i < N ; i++){
+            resultado[i] = 0;
+            #pragma acc loop
+            for(int j=0 ; j < (row_ptr[i+1] - row_ptr[i]) ; j++){
+                resultado[i] += val[indice] * u[ col_ind[indice]-1 ];
+                indice++;
+            }
         }
+    }            
     }
+    /*
     // imprimir resultado
     for(int i=0; i < N; i++){
         printf("resultado[%d] = %d\n", i, resultado[i]);
     }
+    */
+    double runtime = GetTimer();
+    printf(" total: %f s\n", runtime / 1000.f);
+    
+    free(val);
+    free(col_ind);
+    free(row_ptr);
+    free(u);
+    free(resultado);
 }
---
+++ Tarea 3/mm3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "timer.h"
+#define N 500
+#define M 500
+#define L 1
+
+int main(){
+
+	float a[N*M], b[M*L], c[N*L];
+	float suma;
+
+	memset(a, 0, N*M*sizeof(float));
+	memset(b, 0, M*L*sizeof(float));
+  	memset(c, 0, N*L*sizeof(float));
+
+	for(int i = 0 ; i < N ; i++) {
+		for(int j=0 ; j < M ; j++){
+			if( i == j ){
+				a[j + i*M] = 1.0f;
+			}
+		}
+	}
+
+	for(int i = 0 ; i < M*L ; i++) {
+    	b[i] = 1.0f;
+	}
+
+	StartTimer();
+	#pragma acc data copyin(a, b), copyout(c)
+	{
+	#pragma acc parallels
+	{
+		#pragma acc loop
+		for (int i = 0; i < N; i++){
+			#pragma acc loop
+			for (int j = 0; j < L; j++){
+				suma = 0.0;
+				#pragma acc loop
+				for (int k = 0; k < M; k++){
+					suma += a[k+i*M] * b[j+k*L];
+				}
+				c[j+i*L] = suma;
+			}
+		}
+	}
+	}
+	/*
+	for (int i= 0; i < N*L; i++){
+		printf("c[%i]: %f\n", i, c[i]);
+	}
+	*/	
+	double runtime = GetTimer();
+	printf(" total: %f s\n", runtime / 1000.f);
+  	return 0;
+}
